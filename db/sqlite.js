7f58ae8e5cdc98bb2369900d16b1a7c2aa5f8a82
Glitch (glitch-blank-sqlite) <none>
2021-12-24 16:09:58 +0000

    ⛸🚓 Checkpoint ./db/messages.js:21878340/152 ./db/sqlite.js:21878340/5528 ./server.js:21878340/44 ./db/index.js:21878340/135

--- db/sqlite.js
+++ db/sqlite.js
@@ -11,7 +11,6 @@ const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const dbWrapper = require("sqlite");
 const faker = require("faker");
-const messages = require("./messages");
 let db;
 
 //SQLite wrapper for async / await connections https://www.npmjs.com/package/sqlite
@@ -40,6 +39,51 @@ dbWrapper
     }
   });
 
-// Server script calls these methods to connect to the db
-module.exports = {...messages};
+module.exports = {
+  // Get the messages in the database
+  getMessages: async () => {
+    try {
+      return await db.all("SELECT * from Messages");
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  },
 
+  // Add new message
+  addMessage: async message => {
+    let success = false;
+    try {
+      success = await db.run("INSERT INTO Messages (message) VALUES (?)", [
+        message
+      ]);
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  },
+
+  // Update message text
+  updateMessage: async (id, message) => {
+    let success = false;
+    try {
+      success = await db.run(
+        "Update Messages SET message = ? WHERE id = ?",
+        message,
+        id
+      );
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  },
+
+  // Remove message
+  deleteMessage: async id => {
+    let success = false;
+    try {
+      success = await db.run("Delete from Messages WHERE id = ?", id);
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  }};
